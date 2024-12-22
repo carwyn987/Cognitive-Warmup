c21957acd5abb4a2a33cf1e92941ff76391e6395
carwyn <carwyncollinsworth@gmail.com>
2024-12-22 15:18:10 -0700

    Updated dir name

--- code/python_generator/run.sh
+++ code/python_generator/run.sh
@@ -2,6 +2,6 @@ docker run --rm \
     -e "OPENAI_API_KEY=$OPENAI_API_KEY" \
     -e "YOUTUBE_API_KEY=$YOUTUBE_API_KEY" \
     -v $(pwd)/source:/app/source:ro \
-    -v $(pwd)/generated:/app/output \
+    -v $(pwd)/output:/app/output \
     -v $(pwd)/config:/app/config:ro \
     python_generator
