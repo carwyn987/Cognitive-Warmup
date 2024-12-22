383921531f4dd325a7f26e6180b1f26e4881fc7c
carwyn <carwyncollinsworth@gmail.com>
2024-12-22 15:37:29 -0700

    Removed dir listing

--- code/python_generator/main.py
+++ code/python_generator/main.py
@@ -16,18 +16,6 @@ parser.add_argument('output_dir', type=str, help='Path to the output directory.'
 parser.add_argument('--config_file', type=str, default="../../config/tasking.yaml", help='Path to the config file.')
 args = parser.parse_args()
 
-def print_dir_tree(start_path, indent=""):
-    for item in os.listdir(start_path):
-        item_path = os.path.join(start_path, item)
-        print(f"{indent}- {item}")
-        if os.path.isdir(item_path):
-            print_dir_tree(item_path, indent + "  ")
-
-# Replace '.' with your desired start directory
-start_directory = "."
-print(f"Directory tree for: {os.path.abspath(start_directory)}")
-print_dir_tree(start_directory)
-
 # Load source files
 myfiles = []
 for root, dirs, files in os.walk(args.source_dir):
