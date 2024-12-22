a4f80ec05f800cb6df1247c0a11262c0e6cf695f
carwyn <carwyncollinsworth@gmail.com>
2024-12-22 15:07:38 -0700

    Restructured

--- code/python_generator/run.sh
+++ code/python_generator/run.sh
@@ -1,7 +1,7 @@
 docker run --rm \
     -e "OPENAI_API_KEY=$OPENAI_API_KEY" \
     -e "YOUTUBE_API_KEY=$YOUTUBE_API_KEY" \
-    -v $(pwd)/../../source:/app/source:ro \
-    -v $(pwd)/../../generated:/app/output \
-    -v $(pwd)/../../config:/app/config:ro \
+    -v $(pwd)/source:/app/source:ro \
+    -v $(pwd)/generated:/app/output \
+    -v $(pwd)/config:/app/config:ro \
     python_generator
